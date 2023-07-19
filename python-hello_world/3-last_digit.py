88518a23816ca0ae39cc68cf789e384c45ed2c38
VitomsCatena <vitomscatenathe1@gmail.com>
2023-07-19 08:54:17 +0300

    task3 another trial

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,6 +4,7 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
-suffix = "greater than 5" if last_digit > 5 else "less than 6 and not 0" if last_digit != 0 else "0"
+comparison = "greater than 5" if last_digit > 5 else "0" if last_digit == 0 else "less than 6 and not 0"
 
-print("Last digit of {}, is {} and is {}".format(number, last_digit, suffix))
+print("Last digit of {}, is {} and is {}".format(number, last_digit, comparison))
+
