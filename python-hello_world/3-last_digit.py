2e8b1473fe00e5212702dd897d9e8ba45db89d7d
VitomsCatena <vitomscatenathe1@gmail.com>
2023-07-19 08:47:20 +0300

    3-last-digit task

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,10 +4,6 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
+suffix = "greater than 5" if last_digit > 5 else "less than 6 and not 0" if last_digit != 0 else "0"
 
-if last_digit > 5:
-    print("Last digit of {}, is {} and is greater than 5".format(number, last_digit))
-elif last_digit == 0:
-    print("Last digit of {}, is {} and is 0".format(number, last_digit))
-else:
-    print("Last digit of {}, is {} and is less than 6 and not 0".format(number, last_digit))
+print("Last digit of {}, is {} and is {}".format(number, last_digit, suffix))
