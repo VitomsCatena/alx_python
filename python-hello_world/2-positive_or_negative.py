96e0e0361ef74ecf625128dee83966db53df864b
VitomsCatena <vitomscatenathe1@gmail.com>
2023-07-19 08:10:42 +0300

    positive 2 py task

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,11 +3,9 @@ import random
 
 number = random.randint(-10, 10)
 
-if number > 0:
-    print("The number is positive")
-elif number == 0:
-    print("The number is zero")
-else:
-    print("The number is negative")
+sign = (number > 0) - (number < 0)
+signs = {1: "positive", 0: "zero", -1: "negative"}
 
+print("The number is {}".format(signs[sign]))
 print(number)
+
