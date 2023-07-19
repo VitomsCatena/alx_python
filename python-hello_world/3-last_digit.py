86cdc1c20ede2aa7b0d61aab00a89fa056287517
VitomsCatena <vitomscatenathe1@gmail.com>
2023-07-19 08:44:58 +0300

    assign a random signed number to the variable

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,15 +3,11 @@ import random
 
 number = random.randint(-10000, 10000)
 
-# Extract the last digit of the number
 last_digit = abs(number) % 10
 
-# Print the result based on the last digit
-print("The string Last digit of,", number, "is", last_digit, end=" ")
-
 if last_digit > 5:
-    print("and is greater than 5")
+    print("Last digit of {}, is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
-    print("and is 0")
+    print("Last digit of {}, is {} and is 0".format(number, last_digit))
 else:
-    print("and is less than 6 and not 0")
+    print("Last digit of {}, is {} and is less than 6 and not 0".format(number, last_digit))
