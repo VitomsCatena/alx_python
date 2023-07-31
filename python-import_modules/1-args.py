e38472e766e9385777429ee294d793343f2f065a
VitomsCatena <vitomscatenathe1@gmail.com>
2023-07-31 19:38:10 +0300

    correction

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,14 @@
+#!/usr/bin/env python3
+
 import sys
 
-def print_arguments():
-    # Get the number of arguments
-    num_args = len(sys.argv) - 1
-
-    # Print the number of arguments
-    print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}")
-
-    # If no arguments were passed, print '.' and a new line
-    if num_args == 0:
-        print(".")
-    else:
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
-
-# Check if the script is being run directly
 if __name__ == "__main__":
-    print_arguments()
+    num_args = len(sys.argv) - 1
+    args_list = sys.argv[1:]
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''))
+        for i, arg in enumerate(args_list, start=1):
+            print("{}: {}".format(i, arg))
