d1352afade22509dfebc6b2dfd68e0851fc26a03
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-08-28 10:11:28 +0300

    Create 0-hello_route.py

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""me trying to  Writing the first route in flask"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def task_0():
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
