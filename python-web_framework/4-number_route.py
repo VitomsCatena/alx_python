9212b7d1bb5adf9acd03fdccbe78044cf0ce5783
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-08-28 10:21:56 +0300

    Create 4-number_route.py

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""  five routes  """
+
+from flask import Flask, abort
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def task_0():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def task_1():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def task_2(text):
+    word = text.split('_')
+    return f"C {' '.join(word)}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def task_3(text='is cool'):
+    word = text.split('_')
+
+    return f"Python {' '.join(word)}"
+
+
+# Display if n is integer
+@app.route('/number/<int:n>', strict_slashes=False)
+def task_4(n):
+    return f'{n} is a number'
+
+
+# Display if n is float
+@app.route('/number/<float:n>', strict_slashes=False)
+def task_4_1(n):
+    abort(404)
+
+
+# Display if n is string
+@app.route('/number/<string:n>', strict_slashes=False)
+def task_4_2(n):
+    abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
