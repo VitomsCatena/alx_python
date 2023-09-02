6b0cc4697bb335b6fa2e4cfe660d89b68308e262
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-02 10:49:23 +0300

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""
-3-base_geometry module
+BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
-Define an empty class BaseGeometry.
-"""
-
-
-class BaseGeometry:
-    """
-    An empty class representing BaseGeometry.
-    """
-
-    def area(self):
-        """
-        Public instance method that raises an Exception with the message 'area() is not implemented'.
-        """
-        raise Exception("area() is not implemented")
-
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-    bg.area()
+bg = BaseGeometry()
+print(dir(bg))
