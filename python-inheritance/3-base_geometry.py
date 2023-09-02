dd1719db1061eb3de83a3a8fe42ac6c75f2912a1
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-02 10:41:29 +0300

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-#!/usr/bin/python3
 """
 3-base_geometry module
 
@@ -38,31 +36,3 @@ if __name__ == "__main__":
     print(BaseGeometry)
     print(class_dir(BaseGeometry))
 
-
-
-
-
-"""
-3-base_geometry module
-
-Define an empty class BaseGeometry.
-"""
-
-class BaseGeometry:
-    """
-    An empty class representing BaseGeometry.
-    """
-
-    def __init_subclass__(cls):
-        """
-        Override the __init_subclass__ method to exclude it from the list of attributes for the class.
-        """
-        pass
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-    print(bg)
-    print(dir(bg))
-
-    print(BaseGeometry)
-    print(dir(BaseGeometry))
