8e043145682373f059c0899be1f9cf9fa37114ca
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-16 09:10:37 +0300

    Updated 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
-"""Empty BaseGeometry class"""
 
+"""
+This module is an empty class 
+"""
+class OverrideMetaClass(type):
+    """
+    Override original inherited attributes from parent
+    """
+    def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
 
-class BaseGeometry:
-    """Class Geometry"""
-
+class BaseGeometry(metaclass=OverrideMetaClass):
+    """
+    This class models an empty class
+    """
+    def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
     def area(self):
-        """Raises Exception only"""
-        raise Exception('area() is not implemented')
-
+        """a method to raise an exception with a message"""
+        raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
-        """Integer validator if less than 0 or not int"""
-        if type(value) != int:
+        """
+        A method that validates value
+        """
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
