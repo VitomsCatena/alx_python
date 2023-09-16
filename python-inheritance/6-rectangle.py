13f1714ee0093f0b56d93afebd653d5d54ab0842
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-16 09:03:08 +0300

    Updated 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,82 +1,45 @@
 #!/usr/bin/python3
-"""Define a rectangle"""
 
+"""
+Module containing the Rectangle class.
+"""
+class OverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
 
-class Rectangle:
-    """Creates a rectangle."""
-
-    number_of_instances = 0
-
-    def __init__(self, width=0, height=0):
-        """Initialize data."""
-        self.width = width
-        self.height = height
-        Rectangle.number_of_instances += 1
-
-    def __str__(self):
-        """Returns the rectangle with the character #"""
-        if self.__width == 0 or self.__height == 0:
-            return ''
-        rectangle = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rectangle += '#'
-            rectangle += '\n'
-        return rectangle[:-1]
-
-    def __repr__(self):
-        """Return a string representation of the rectangle to be able
-        to recreate a new instance by using eval() (see example below)
+    def __dir__(cls):
         """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """Print the message Bye rectangle...
-        (... being 3 dots not ellipsis) when an
-        instance of Rectangle is deleted"""
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
-
-    @property
-    def width(self):
-        """Retrieves the width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Sets the width of a Rectangle instance
-        Args:
-            value: value of width, must be >=0
+        Returns:
+            list: List of attributes excluding __init_subclass__.
         """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+    
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-    @property
-    def height(self):
-        """Retrieves the height."""
-        return self.__height
 
-    @height.setter
-    def height(self, value):
-        """Sets the height of a Rectangle instance
-        Args:
-            value: value of height, must be >=0
+class Rectangle(BaseGeometry, metaclass=OverrideMetaClass):
+    """
+    Models a rectangle. A derived class of BaseGeometgry
+    """
+    def __dir__(cls) -> None:
         """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """returns the rectangle area."""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """Returns the rectangle perimeter"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+        Removes __init_subclass__ from list of class attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
+    def __init__(self, width, height):
+        """
+        Call attriutes of parent.
+        validates attributes for positive integer
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
