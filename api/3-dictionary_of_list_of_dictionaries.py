0c1352f18600d07dbe53633f1c97f6f580cc02c1
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-10-07 16:45:33 +0300

    Updated 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,58 @@
+import json, requests, sys
 
-import json
-import requests
-import sys
+def get_employee_data(employee_id):
+    # URL to fetch employee details for all employees
+    employee_url = f"https://jsonplaceholder.typicode.com/users"
+    
+    try:
+        # Fetch data for all employees
+        response_employee = requests.get(employee_url)
+        response_employee.raise_for_status()
+        employee_data = response_employee.json()
+        
+        all_data = {}
+        
+        # Iterate over all employees
+        for employee in employee_data:
+            employee_id = employee["id"]
+            employee_name = employee["name"]
+            
+            # Fetch to do list
+            _, todos_data = fetch_todos(employee_id)
+            
+            # Extract completed tasks for the employee
+            completed_tasks = [{"username": employee_name, "task": todo["title"], "completed": todo["completed"]} for todo in todos_data if todo["completed"]]
+            
+            all_data[employee_id] = completed_tasks
+            
+        return all_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+        
+def fetch_todos(employee_id):
+    # URL to fetch employee's to do list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    try:
+        response_todos = requests.get(todos_url)
+        response_todos.raise_for_status()
+        todos_data = response_todos.json()
+        
+        return todos_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+        
+def main():
+    all_data = get_employee_data()
+    
+    # Export data to json
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_data, json_file)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 3-dictionary_of_list_of_dictionaries.py <employee_id>")
-        sys.exit(1)
+    main()
 
-    employee_id = sys.argv[1]
-
-    # Fetch user data
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    user_data = user_response.json()
-    username = user_data["username"]
-
-    # Fetch tasks data for the given user
-    tasks_response = requests.get("https://jsonplaceholder.typicode.com/todos")
-    tasks_data = tasks_response.json()
-
-    # Create a dictionary to store tasks for all users
-    all_tasks = {}
-
-    # Iterate through tasks to organize them by user
-    for task in tasks_data:
-        user_id = task["userId"]
-        if user_id not in all_tasks:
-            all_tasks[user_id] = []
-
-        task_data = {
-            "username": username,
-            "task": task["title"],
-            "completed": task["completed"],
-        }
-
-        all_tasks[user_id].append(task_data)
-
-    # Export the data to a JSON file
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_tasks, json_file)
+   
+     
